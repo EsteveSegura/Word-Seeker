1e042a2d6eb0767382717b11d94efd785f4d99ef
Esteve <estevesegura.gir@gmail.com>
2020-12-31 01:24:31 +0100

    delete oldbackend

--- oldback/src/middleware/login.js
+++
@@ -1,7 +0,0 @@
-function checkLogin(req,res,next){
-    if(req.session.twitchData) next()
-    console.log(req.session)
-    if(!req.session.hasOwnProperty('twitchData')) res.status(401).json({error: "Unauthorized"})
-}
-
-module.exports = { checkLogin }
